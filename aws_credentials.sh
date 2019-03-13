7ac366243170c3701436666738d9ebb689435fd8
Zichen Li <zichenli6@gmail.com>
2019-03-13 19:46:18 -0400

    update aws credentials

--- aws_credentials.sh
+++ aws_credentials.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/demo/.aws
+mkdir -p ~/.aws
 
-cat > ~/demo/.aws/credentials << EOL
+cat > ~/.aws/credentials << EOL
 [default]
 aws_access_key_id = ${AWS_ACCESS_KEY_ID}
 aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
