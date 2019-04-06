a47cb8b5cc5e714e58860419a05f40ec75d775ec
khoojinghwan <hao.07@hotmail.com>
2019-04-06 16:02:42 -0400

    drew rectangle over face

--- app/client/src/components/home/uploadDropzone.js
+++ app/client/src/components/home/uploadDropzone.js
@@ -10,27 +10,60 @@ class UploadDropzone extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            newImage: null, // uploaded image, to be sent to backend
-            displayImg: null // uploaded image, parsed with FileReader to be displayed
+            newImage: null
         };
     }
 
+    // componentDidMount() {
+    //     this.updateCanvas();
+    // }
+    //
+    // componentDidUpdate() {
+    //     this.updateCanvas();
+    // }
+
+    // updateCanvas() {
+    //     const ctx = this.refs.canvas.getContext('2d');
+    //     ctx.fillRect(0,0, 100, 100);
+    //
+    //     // const ctx = this.refs.canvas.getContext('2d');
+    //     // ctx.clearRect(0,0, 300, 300);
+    //     // // draw children “components”
+    //     // rect({ctx, x: 10, y: 10, width: 50, height: 50});
+    //     // rect({ctx, x: 110, y: 110, width: 50, height: 50});
+    // }
+
     handleDrop = (files) => {
-        this.setState({ newImage: files[0] });
+        this.drawCanvas(files[0]);
     }
 
     handleChangeImageUpload = (e) => {
-        var file = e.target.files[0];
+        this.drawCanvas(e.target.files[0]);
+    };
+
+    drawCanvas = (file) => {
         var reader = new FileReader();
         reader.readAsDataURL(file);
+
+        const canvas = this.refs.canvas;
+        const ctx = canvas.getContext('2d');
+
         reader.onloadend = function (e) {
-            console.log("ONLOADEND")
             this.setState({
-                displayImg: [reader.result],
                 newImage: file
             })
+
+            var img = new Image();
+            img.onload = function(){
+                canvas.width = img.width;
+                canvas.height = img.height;
+                ctx.drawImage(img,0,0);
+                ctx.rect(50, 20, 75, 75);
+                ctx.stroke();
+            }
+            img.src = e.target.result;
         }.bind(this);
-    };
+    }
 
     handleClickImageUpload = (e) => {
         e.preventDefault();
@@ -49,13 +82,13 @@ class UploadDropzone extends Component {
                         <DragAndDrop handleDrop={this.handleDrop}>
                             <div className="dropzone-content">
                                 <div className="dropzone-content-text">
+                                    <canvas ref="canvas" id="uploaded-img"/>
                                     {
-                                        this.state.displayImg ?
-                                            <img src={this.state.displayImg} alt="user upload" id="uploaded-img"/>:
+                                        !this.state.newImage ?
                                             <div>
                                                 <div id="upload-icon"><FaFileUpload /></div>
                                                 <div>DRAG FILE TO UPLOAD</div>
-                                            </div>
+                                            </div> : null
                                     }
                                 </div>
                             </div>
