38aa7c8ceb138c1bd6048a081fbcc2dfe0fc900f
Kayli O'Keefe <ko733@nyu.edu>
2019-04-06 17:04:51 -0400

    reformat nav bar

--- app/client/src/components/navbar.js
+++ app/client/src/components/navbar.js
@@ -7,7 +7,7 @@ class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <Link to="/" className="navbar-brand">RIS</Link>
+                <Link to="/home" className="navbar-brand">R.I.S.E</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
