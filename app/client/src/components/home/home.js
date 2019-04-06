75db035aee5dfc82052eb1e80f457afed204b5a8
Kayli O'Keefe <ko733@nyu.edu>
2019-04-06 16:46:20 -0400

    add logo to home page and place images in correct directory

--- app/client/src/components/home/home.js
+++ app/client/src/components/home/home.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import './home.css';
 import { connect } from 'react-redux';
 import UploadDropzone from './uploadDropzone.js';
-
 class Home extends Component {
 
     render() {
         return (
             <div className="App container">
                 <h1>Welcome to RIS!</h1>
+
+                <img src={require('./logo-designs/logo-image.png')} alt='logo' />
+                <button type="button" className="btn btn-primary btn-lg">Large button</button>
+
+
                 <div><UploadDropzone /></div>
             </div>
         );
