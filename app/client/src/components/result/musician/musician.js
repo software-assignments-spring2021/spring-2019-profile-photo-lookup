dc3e4f88d2c41af9b3387922ab37d7fa06d0c8e5
khoojinghwan <hao.07@hotmail.com>
2019-04-10 15:03:20 -0400

    added temp url to stop spotify playlist from breaking

--- app/client/src/components/result/musician/musician.js
+++ app/client/src/components/result/musician/musician.js
@@ -5,16 +5,16 @@ import { connect } from 'react-redux';
 
 class Musician extends Component {
 
-    // constructor(props) {
-    //     super(props);
-        // this.state = {
-        //     url: "https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3"
-        // };
-    // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: "https://open.spotify.com/embed/album/1DFixLWuPkv3KT3TnV35m3"
+        };
+    }
 
     render() {
         return (
-            <SpotifyPlaylist url={this.props.info.url}/>
+            <SpotifyPlaylist url={this.state.url}/>
         );
     }
 
