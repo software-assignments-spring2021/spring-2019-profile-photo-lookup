fd1426c75b5566c562c82f2fc1d1b94503088f44
Kayli O'Keefe <ko733@nyu.edu>
2019-04-12 14:19:52 -0400

    extract actor id

--- app/server/crawler/actor.py
+++ app/server/crawler/actor.py
@@ -1,10 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
-html = requests.get('https://www.imdb.com/name/nm0000130/').content
-page = BeautifulSoup(html, features="html.parser")
 
-print(page)
+
+def getNameID(name):
+       
+    html = requests.get('https://www.imdb.com/find?q=Tom+Cruise=nm').content
+
+    page = BeautifulSoup(html, features="html.parser")
+    person = page.find("td", {"class": "result_text"}).contents[1]
+    id = str(person)
+    return id.split('/')[2]
+
+def getActorInfo(id):
+       
+    html = requests.get('https://www.imdb.com/name/%7Bid%7D').content
+
+page = BeautifulSoup(html, features="html.parser")
+person = page.find("td", {"class": "result_text"}).contents[1]
+person2 = str(person)
+print(person2.split('/')[2])
 
 titles = page.findAll("div", {"class": "knownfor-title-role"})
-for title in titles:
-    print(title.contents[1].contents)
+#for title in titles:
+   # print(title.contents[1].contents)
