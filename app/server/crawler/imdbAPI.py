89a5c5613c9f26c8832ebd530115f6bfbb761457
Kayli O'Keefe <ko733@nyu.edu>
2019-04-12 12:38:46 -0400

    crawler works

--- app/server/crawler/imdbAPI.py
+++ app/server/crawler/imdbAPI.py
@@ -1,7 +1,9 @@
 from imdb import IMDb
+from urllib.request import urlretrieve
+import requests
 
-# create an instance of the IMDb class
-ia = IMDb()
+url = "https://api.themoviedb.org/3/search/person?api_key=4567404023e93988b15756b26b82c5ee&language=en-US&query=Tom%20Cruise&page=1&include_adult=falseprint"
+data = requests.Session().get(url=url).json()
+print(data)
 
-
-print(ia.get_person('0000210'))
+ 
