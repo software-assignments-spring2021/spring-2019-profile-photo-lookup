fbe4c6ac30a165e162b20da4909e8d14219b9720
Kayli O'Keefe <ko733@nyu.edu>
2019-04-12 14:22:12 -0400

    small parsing change

--- app/server/crawler/actor.py
+++ app/server/crawler/actor.py
@@ -2,24 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def getNameID(name):
+
        
-    html = requests.get('https://www.imdb.com/find?q=Tom+Cruise=nm').content
-
-    page = BeautifulSoup(html, features="html.parser")
-    person = page.find("td", {"class": "result_text"}).contents[1]
-    id = str(person)
-    return id.split('/')[2]
-
-def getActorInfo(id):
-       
-    html = requests.get('https://www.imdb.com/name/%7Bid%7D').content
-
+html = requests.get('https://www.imdb.com/find?q=Tom+Cruise=nm').content
 page = BeautifulSoup(html, features="html.parser")
 person = page.find("td", {"class": "result_text"}).contents[1]
-person2 = str(person)
-print(person2.split('/')[2])
+nameID = str(person)
+id_num = nameID.split('/')[2]
+print(id_num)
 
-titles = page.findAll("div", {"class": "knownfor-title-role"})
+
+#titles = page.findAll("div", {"class": "knownfor-title-role"})
 #for title in titles:
    # print(title.contents[1].contents)
