47fd8f07a16c34f6a264872c587957144f5a19cf
Kayli O'Keefe <ko733@nyu.edu>
2019-04-12 19:02:28 -0400

    update actor crawler to return a list of known titles for a given name

--- app/server/crawler/actor.py
+++ app/server/crawler/actor.py
@@ -1,17 +1,31 @@
 import requests
+from requests.models import PreparedRequest
 from bs4 import BeautifulSoup
 
 
+def getActorID(name):
+   nameSplit = name.split(" ")
+   query = nameSplit[0] + nameSplit[1]
+   req = PreparedRequest()
+   base_url = "https://www.imdb.com/find"
+   params = {query: 'nm'}
+   req.prepare_url(base_url, params)   
+   html = requests.get(req.url).content 
+   page = BeautifulSoup(html, features="html.parser")
+   person = page.find("td", {"class": "result_text"}).contents[1]
+  # print(person)
+   nameID = str(person)
+   id_num = nameID.split('/')[2]
+   return id_num
 
-       
-html = requests.get('https://www.imdb.com/find?q=Tom+Cruise=nm').content
-page = BeautifulSoup(html, features="html.parser")
-person = page.find("td", {"class": "result_text"}).contents[1]
-nameID = str(person)
-id_num = nameID.split('/')[2]
-print(id_num)
+
+def getKnownTitles(actorID):
+   url = 'https://www.imdb.com/name/'+actorID
+   html = requests.get(url).content
+   actor_page = BeautifulSoup(html, features="html.parser")
+   titles = actor_page.findAll("div", {"class": "knownfor-title-role"})
+   return titles
 
 
-#titles = page.findAll("div", {"class": "knownfor-title-role"})
-#for title in titles:
-   # print(title.contents[1].contents)
+
+
