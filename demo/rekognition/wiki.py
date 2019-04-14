535939ffe07784025301d2cdeada4e3b25bde4a2
Wei Jie Chua <wjc354@nyu.edu>
2019-04-14 19:18:59 -0400

    added athlete.py

--- demo/rekognition/wiki.py
+++ demo/rekognition/wiki.py
@@ -1,23 +1,32 @@
+import athlete as Athlete
 import wikipedia
 import numpy as np
 
 titles = ['David Beckham', 'Michael Jordan']
 
-possibles = ['Award', 'Awards', 'Honours', 'Honors', 'Honour', 'Honor','Accolade']
 
-career_possibles = ['Career statistics', 'Statistics', 'career statistics']
+def getPlot(names):
+    possibles = ['Award', 'Awards', 'Honours', 'Honors', 'Honour', 'Honor','Accolade']
 
-for i in titles:
-    try:
-        wik = wikipedia.WikipediaPage(i[0])
-    except:
-        wik = np.NaN
+    career_possibles = ['Career statistics', 'Statistics', 'career statistics']
 
-    try:
-        for j in possibles:
-            if wik.section(j) != None:
-                plot = wik.section(j).replace('\n', '').replace('\n', '')
-    except:
-        plot = np.Nan   
+    for i in names:
+        try:
+            wik = wikipedia.WikipediaPage(i[0])
+        except:
+            wik = np.NaN
 
-                                                        
+        try:
+            for j in possibles:
+                if wik.section(j) != None:
+                    plot = wik.section(j).replace('\n', '').replace('\n', '')
+        except:
+            plot = np.Nan
+
+
+def findPlot(name):
+    name_with_underscore,url_link = athlete.getAthleteName(name)
+    summary = wikipedia.WikipediaPage(title=name_with_underscore).summary
+
+
+print(findPlot("Lee Chong Wei"))
