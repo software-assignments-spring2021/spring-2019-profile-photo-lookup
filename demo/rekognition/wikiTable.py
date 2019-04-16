967f6f3aa76f5ff88df02962e05120cedbc77903
Wei Jie Chua <wjc354@nyu.edu>
2019-04-16 16:52:29 -0400

    formatting table

--- demo/rekognition/wikiTable.py
+++ demo/rekognition/wikiTable.py
@@ -5,11 +5,21 @@ import athlete as Athlete
 
 
 def createTable(name):
+    result = []
+    result_head = []
+    result_info = []
     name_with_underscore,url_link = Athlete.getAthleteName(name)
     soup = Athlete.getPage(name)
     table = soup.find('table', {'class': 'wikitable'})
-    table_info = table.find_all('tr')
-    print(table_info)
+    table_header = table.find_all('th')
+    table_info = table.find_all('td')
+
+    for head in table_header:
+        print(type(head))
+        head = head.replace('<th>',"")
+        head = head.replace('</th>',"")
+        result_head.append(head)
+    print(result_head)
 
 
 createTable('Lee Chong Wei')
