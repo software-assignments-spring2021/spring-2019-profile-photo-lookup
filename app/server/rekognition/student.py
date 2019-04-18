8cb54342599a5bb299d91d26cf024661e0394244
Zichen Li <zichenli6@gmail.com>
2019-04-18 14:21:18 -0400

    Improve exception and error handling for student rekognition

--- app/server/rekognition/student.py
+++ app/server/rekognition/student.py
@@ -9,9 +9,7 @@ def filter_name(name):
     lst = name.split('-')
     if len(lst[0]) == 1:
         name = re.sub('-', '. ', name)
-
     return name
-
 
 def construct_student_profile(ID):
     info = ID.split('_')
@@ -24,21 +22,19 @@ def construct_student_profile(ID):
     }
     return student
 
-
+# calculate extended corner locations for face crop
 def crop_face(x1, y1, x2, y2, img):
-    # calculate extended corner locations
-    # enlarge face crop to include the entire head
-    hy1 = int(y1 - (0.2 * (y2 - y1)))
+    ratio = 0.5 # enlarge face crop to include the entire head
+    hy1 = int(y1 - (ratio * (y2 - y1)))
     if hy1 <= 0:
         hy1 = int(y1)
     hy2 = int(y2)
-    hx1 = int(x1 - (0.2 * (x2 - x1)))
+    hx1 = int(x1 - (ratio * (x2 - x1)))
     if hx1 <= 0:
         hx1 = int(x1)
-    hx2 = int(x2 + (0.2 * (x2 - x1)))
+    hx2 = int(x2 + (ratio * (x2 - x1)))
     if hx2 <= 0:
         hx2 = int(x2)
-
     return img[hy1:hy2, hx1:hx2]
 
 
@@ -59,7 +55,6 @@ def detect_faces(filename):
         face = crop_face(left, top, left + width, top + height, image)
         face_bytes = cv2.imencode('.jpg', face)[1].tostring()
         faces.append(face_bytes)
-
     return faces
 
 
@@ -69,13 +64,16 @@ def rekognize_student(img_path):
     for face_bytes in faces:
         response = client.search_faces_by_image(CollectionId = 'RISE_NYU',
                                                 Image = {'Bytes': face_bytes},
-                                                FaceMatchThreshold = 80,
+                                                FaceMatchThreshold = 70,
                                                 MaxFaces = 1)
-        match = response['FaceMatches'][0]
-        uid = match['Face']['ExternalImageId']
-        profile = construct_student_profile(uid)
-        profile['bbox'] = match['Face']['BoundingBox']
-        profile['confidence'] = "{:.2f}".format(match['Similarity']) + "%"
-        output.append(profile)
+        if response['FaceMatches'] != []:
+            match = response['FaceMatches'][0]
+            ID = match['Face']['ExternalImageId']
+            profile = construct_student_profile(ID)
+            profile['bbox'] = match['Face']['BoundingBox']
+            profile['confidence'] = "{:.2f}".format(match['Similarity']) + "%"
+            output.append(profile)
+        else:
+            print("No Matches")
 
     return {"students": output}
