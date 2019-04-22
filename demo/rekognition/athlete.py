e9b71d32944f2df24dd56d6de2ae3873764a5da8
Wei Jie Chua <wjc354@nyu.edu>
2019-04-22 19:16:51 -0400

    refactor athlete.py to include prototype

--- demo/rekognition/athlete.py
+++ demo/rekognition/athlete.py
@@ -2,6 +2,13 @@ import requests
 from requests.models import PreparedRequest
 from bs4 import BeautifulSoup
 
+class pageInformation(name):
+    this.name = name
+    this.name_with_underscore = name = name.replace(" ", "_")
+    this.page = getPage(name)
+    name, info = getAthleteName(name)
+    this.link = info
+
 def getAthleteName(name):
     name = name.replace(" ", "_")
     request = PreparedRequest()
@@ -10,12 +17,19 @@ def getAthleteName(name):
     url_link = "https://en.wikipedia.org/wiki/" + name
     return name, url_link
 
+#print(getAthleteName("Lee Chong Wei"))
 
-# This can be used as prototype. A wikipedia page will be retrieved first before used
-# by other classes
+# Web crawl once so that dont have to do it twice
 def getPage(name):
     name = name.replace(" ", "_")
     url_link = "https://en.wikipedia.org/wiki/" + name
     website_url = requests.get(url_link).text
     soup = BeautifulSoup(website_url, 'lxml')
     return(soup)
+
+def getLink(name):
+    request = PreparedRequest()
+    request.prepare_url("https://en.wikipedia.org/wiki/", name)
+
+    url_link = "https://en.wikipedia.org/wiki/" + name
+    return url_link
