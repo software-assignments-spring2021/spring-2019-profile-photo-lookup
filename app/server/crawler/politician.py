544c57f76a3e31fdac919416f9ad8155678e7225
Amanda Labuda <arl563@nyu.edu>
2019-04-23 12:28:14 -0400

    fixed politician bug

--- app/server/crawler/politician.py
+++ app/server/crawler/politician.py
@@ -1,160 +1,45 @@
-import requests
-from requests.models import PreparedRequest
-from bs4 import BeautifulSoup
-from .wikiAPI import search_wiki
+from .politicianstrategy import HouseRepStrategy
+from .politicianstrategy import SenateRepStrategy
+from .politicianstrategy import ExecBranchStrategy
 from .celebrity import Celebrity
+from .wikiAPI import search_wiki
 
+house_rep = HouseRepStrategy()
+senate_rep = SenateRepStrategy()
+exec_branch = ExecBranchStrategy()
 
 
 class Politician(Celebrity):
-
     def __init__(self, name, occupations):
-        Celebrity.__init__ (self, name, occupations)
+        self.name= name
+        self.occupations= occupations
         self.occID = 'politician'
+        self.member_ID= 0
+        self.strategy= self.determine_strategy()
         self.info = self.retrieve_info()
 
+    def determine_strategy(self):
+        self.strategy= senate_rep
+        member_ID= politician.strategy.find_role()
+        if member_ID== 0:
+            self.strategy= house_rep
+            member_ID= politician.strategy.find_role()
+        self.member_ID= member_ID
+        old_strategy= self.strategy
+        self.strategy= exec_branch
+        check= self.strategy.find_role()
+        if check==1:
+            self.strategy= exec_branch
+        else:
+            self.strategy= old_strategy
+
+        return strategy 
+
     def retrieve_info(self):
-        member_ID = check_Senate(self.name)
-        if member_ID == 0:
-            member_ID= check_House(self.name)
-        info = {}
-        if member_ID != 0:
-            info = construct_congress_profile(member_ID)
-        info = check_execs(self.name)
+        info= self.strategy.construct_profile()
         wiki_data = search_wiki(self.name)
         wiki_desc = wiki_data[2][0]
         info['bio']= wiki_desc
 
         return info
 
-
-
-
-def check_Senate(name):
-
-    new_name = name.split()
-    headers = {"X-API-Key":"KgI2lOueGBFwLYWYsicnT4PSQUblFGDEpfj2Gcdd"}
-
-    member_ID= 0
-    x = 115
-    while x > 79:
-        add_on= str(x)+ "/senate/members"
-        url= "https://api.propublica.org/congress/v1/" + add_on
-        response= requests.get(url, headers=headers)
-        data= response.json()
-
-        first_name= new_name[0]
-        last_name= new_name[1]
-        congresses= data["results"]
-        congress= congresses[0]
-        check_found=0
-        members= congress["members"]
-        for member in members:
-            if(member["last_name"]==last_name):
-                if(member["first_name"]== first_name):
-                    member_ID= member["id"]
-                    check_found=1
-                    break
-        if check_found==1:
-            break
-        x=x-1
-    return member_ID
-
-
-def check_House(name):
-
-    name = name.split(" ")
-    headers = {"X-API-Key":"KgI2lOueGBFwLYWYsicnT4PSQUblFGDEpfj2Gcdd"}
-
-    member_ID= 0
-    x=115
-    while x>102:
-        add_on= str(x)+ "/house/members"
-        url= "https://api.propublica.org/congress/v1/" + add_on
-        response= requests.get(url, headers=headers)
-        data= response.json()
-
-        first_name= name[0]
-        last_name= name[1]
-        congresses= data["results"]
-        congress= congresses[0]
-        check_found=0
-        members= congress["members"]
-        for member in members:
-            if(member["last_name"]==last_name):
-                if(member["first_name"]== first_name):
-                    member_ID= member["id"]
-                    check_found=1
-                    break
-        if check_found==1:
-            break
-        x=x-1
-    return member_ID
-
-
-def construct_congress_profile(member_ID):
-    headers = {"X-API-Key":"KgI2lOueGBFwLYWYsicnT4PSQUblFGDEpfj2Gcdd"}
-    url= "https://api.propublica.org/congress/v1/" + "members/" + member_ID
-    response= requests.get(url, headers=headers)
-    data= response.json()
-    result= data["results"]
-    member= result[0]
-    profile= {}
-    if(member["middle_name"]):
-        profile['name']= member["first_name"] + " " + member["middle_name"] + " " + member["last_name"]
-    else:
-        profile['name']= member["first_name"] + " " +  member["last_name"]
-    profile['party']= ""
-    if member["current_party"]== "R":
-        profile['party']= "Republican"
-    elif member["current_party"]== "D":
-        profile['party']= "Democrat"
-    else:
-        profile['party']= "Other"
-    roles_list= member["roles"]
-    roles= roles_list[0]
-    profile['title']= "U.S. " + roles["chamber"] + ", " + roles["title"]
-    profile['state']= roles["state"]
-    profile['service_span']= "From " + roles["start_date"]+ " to " + roles["end_date"]
-    profile['website']= member["url"]
-    if(member["twitter_account"]):
-        profile['twitter']= "@" + member["twitter_account"]
-    profile['facebook']= member["facebook_account"]
-    profile['address']= roles["office"]
-    profile['phone']= roles["phone"]
-    committees=[]
-    for committee in roles["committees"]:
-        committees.append(committee["name"])
-    profile['committees']=committees
-    return(profile)
-
-def check_execs(name):
-    execs= requests.get("https://theunitedstates.io/congress-legislators/executive.json").json()
-    name= name.split(" ")
-    president={}
-    profile={}
-    for pres in execs:
-        pname= pres["name"]
-        first_name= pname["first"]
-        last_name= pname["last"]
-        if(first_name== name[0] and last_name== name[1]):
-            president=pres
-            full_name= ""
-            fname= president['name']
-            for key in fname:
-                if(key== 'nickname'):
-                    profile['nickname']= fname[key]
-                else:
-                    full_name= full_name + fname[key] + " "
-            profile['name']= full_name
-            profile['birthday']= president['bio']['birthday']
-            profile['service_span']= []
-            for term in president['terms']:
-                if(term['type']=="prez"):
-                    profile['title']= "President of the United States"
-                else:
-                    profile['title']= "Vice President of the United States"
-                term_time= "From "+ term["start"] + " to " + term['end']
-                profile['service_span'].append(term_time)
-                profile['party']= term['party']
-                return profile
