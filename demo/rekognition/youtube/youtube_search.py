75b2ed2baa74228cec5a19116bcd55f31601995c
Wei Jie Chua <wjc354@nyu.edu>
2019-04-25 13:40:53 -0400

    retrieve information from youtube api

--- demo/rekognition/youtube/youtube_search.py
+++ demo/rekognition/youtube/youtube_search.py
@@ -8,7 +8,14 @@ import os
 
 import googleapiclient.discovery
 
-def main(query):
+def main(query, num_result):
+    publishedAt = 0
+    channelId = 0
+    title = ""
+    description = ""
+    thumbnails = {}
+    channelTitle = ""
+    liveBroadcastContent = ""
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -22,14 +29,46 @@ def main(query):
 
     request = youtube.search().list(
         part="snippet",
-        maxResults=1,
+        maxResults=num_result,
         q=query
     )
 
     response = request.execute()
+    #d = {"first_name": "Alfred", "last_name":"Hitchcock"}
+
+    for key,val in response.items():
+        #print(type(key))
+        #print(type(val))
+        if key == "items":
+            #print(type(val[0]))
+            #for i in range(4):
+            for k, v in val[0].items():
+                #print(type(v))
+                print("{} = {}".format(k, v))
+                if k == "snippet":
+                    #print(type(v))
+                    for snippet_key, snippet_value in v.items():
+                        print("Snippet")
+                        print("{} = {}".format(snippet_key, snippet_value))
+                        if snippet_key == "title":
+                            title = snippet_value
+                        elif snippet_key == "publishedAt":
+                            publishedAt = snippet_value
+                        elif snippet_key == "description":
+                            description = snippet_value
+                        elif snippet_key == "thumbnails":
+                            thumbnails = snippet_value
+                        elif snippet_key == "channelTitle":
+                            channelTitle = snippet_value
+                        elif snippet_key == "liveBroadcastContent":
+                            liveBroadcastContent = snippet_value
+
+        else:
+            print("")
+            #print("{} = {}".format(key, val))
     #response = response.encode('utf-8')
     #return response
-    print(response)
+    #print(response)
 
 if __name__ == "__main__":
-    main("David Beckham Highlights")
+    main("David Beckham Highlights", 1)
