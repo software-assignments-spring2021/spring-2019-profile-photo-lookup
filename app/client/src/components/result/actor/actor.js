50d2ce1efaed9a5a07797c72a2a7e40db6de1e0e
khoojinghwan <hao.07@hotmail.com>
2019-04-26 20:30:51 -0400

    actor update

--- app/client/src/components/result/actor/actor.js
+++ app/client/src/components/result/actor/actor.js
@@ -3,9 +3,21 @@ import "./actor.css";
 
 class Actor extends Component {
 
-    render() {
+    renderTitles() {
         return (
-            <h3>This is an actor</h3>
+            <section className="info">
+                <div className="info-header"><div className="info-bubble title-bubble">Titles</div></div>
+                <div className="info-content">{this.props.celeb.info.titles.join(', ')}</div>
+            </section>
+        );
+    }
+
+    render() {
+        let celeb = this.props.celeb;
+        return (
+            <div>
+                {celeb.info.titles ? this.renderTitles() : null}
+            </div>
         );
     }
 }
