87e55b8922fc7509d64222979ad2f2d58934bcaa
Zichen Li <zichenli6@gmail.com>
2019-04-26 21:47:53 -0400

    Implement unit tests for student rekognition

--- app/server/rekognition/tests/test_models.py
+++ app/server/rekognition/tests/test_models.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from ..models import ImageUpload
 from django.core.files.uploadedfile import SimpleUploadedFile
+
+from ..models import ImageUpload
 
 class test_models(TestCase):
     def test_toString(self):
-        test_file = open('rekognition/tests/asset/test_img.jpg', 'rb')
+        test_file = open('rekognition/tests/asset/test_celeb.jpg', 'rb')
         img = SimpleUploadedFile(test_file.name, test_file.read(), content_type="image/jpeg")
         model = ImageUpload(image = img)
         self.assertEqual(str(model), model.image.name)
