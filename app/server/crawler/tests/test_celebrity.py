42d59c71787ec6b06e5214a43d4010ee01d943d6
Amanda Labuda <arl563@nyu.edu>
2019-04-27 17:00:56 -0400

    added celebrity unit test

--- app/server/crawler/tests/test_celebrity.py
+++ app/server/crawler/tests/test_celebrity.py
@@ -1,4 +1,15 @@
 from django.test import TestCase
-from .celebrity import celebrity
+from ..celebrity import Celebrity
 
-Class CelebrityTest(TestCase):
+class CelebrityTest(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(CelebrityTest, self).__init__(*args, **kwargs)
+        self.name = "Bernie Sanders"
+        self.occupations = ["Politician"]
+        self.celebrity = Celebrity(self.name, self.occupations)
+        self.occID= ''
+
+    def test_generateProfile(self):
+        self.celebrity.profile= self.celebrity.generate_profile()
+        test_profile= {'name': 'Bernie Sanders', 'occID': '', 'occupations': 'Politician', 'info': ''}
+        self.assertTrue(self.celebrity.profile==test_profile)
