11e726973ac3bea73c9ee8beb1e3ab61dc60f9d3
Amanda Labuda <arl563@nyu.edu>
2019-04-28 20:19:18 -0400

    created profile unit test

--- app/server/crawler/tests/__init__.py
+++ app/server/crawler/tests/__init__.py
@@ -1,3 +1,4 @@
 from .test_politician import *
 from .test_celebrity import *
+from .test_athlete import *
 from .test_actor import *
---
+++ app/server/crawler/tests/test_profile.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from ..profile import CelebrityFactory
+from .test_poltician import PoliticianTest
+from .test_actor import ActorTest 
+from .test_athlete import AthleteTest
+
+class CelebrityFactoryTest(TestCase):
+    def __init__(self, *args, **kwargs):
+        super(CelebrityFactoryTest, self).__init__(*args, **kwargs)
+        self.names = ["Bernie Sanders", "Tom Cruise", "David Beckham"]
+
+    def test_generateProfile(self):
+        profiles = self.celebrityfactory.construct_profiles()
+        
