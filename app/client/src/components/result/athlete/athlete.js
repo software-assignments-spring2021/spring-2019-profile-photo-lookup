80bb439c01264d17ab49db57efa9ea6990d32679
Kayli O'Keefe <ko733@nyu.edu>
2019-05-05 11:36:07 -0400

    add social media to athlete card

--- app/client/src/components/result/athlete/athlete.js
+++ app/client/src/components/result/athlete/athlete.js
@@ -12,6 +12,8 @@ import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import YouTube from 'react-youtube';
 import _ from "lodash";
+import { SocialIcon } from 'react-social-icons';
+
 
 const styles = theme => ({
     card: {
@@ -27,6 +29,14 @@ const styles = theme => ({
         fontWeight: "bold",
         textAlign: "center",
         paddingBottom: 10
+    },
+    social: {
+        textAlign: "center"
+    },
+    social_icon: {
+        paddingLeft: 5,
+        paddingRight: 10,
+        display: "inline",
     },
     heading: {
         fontSize: 20,
@@ -105,6 +115,23 @@ class AthleteCard extends React.Component {
             );
         }
     }
+    renderSocial() {
+        const classes = this.props.classes;
+        const celeb = this.props.celeb;
+        const twitter = "http://twitter.com/" + celeb.info.twitter
+        const instagram = "http://instagram.com/" + celeb.info.insta
+        return (
+            <div className={classes.social}>
+                <div className={classes.social_icon}>
+                    <SocialIcon url={twitter} />
+                </div>
+                <div className={classes.social_icon}>
+                    <SocialIcon url={instagram} />
+                </div>
+            </div>
+        ); 
+    }
+
 
     render() {
         const classes = this.props.classes;
@@ -126,6 +153,7 @@ class AthleteCard extends React.Component {
                 </Typography>
             </CardContent>
             <CardActions className={classes.actions} disableActionSpacing>
+            {this.renderSocial()}
                 <IconButton
                     className={classnames(classes.expand, {
                     [classes.expandOpen]: this.state.expanded
--- app/server/crawler/athlete.py
+++ app/server/crawler/athlete.py
@@ -1,6 +1,9 @@
 from .celebrity import Celebrity
 from .utilsAPI import WikiAPI, GoogleAPI
-
+from bs4 import BeautifulSoup
+import requests
+import webbrowser
+import urllib
 
 class Athlete(Celebrity):
     def __init__(self, name, occupations):
@@ -12,6 +15,35 @@ class Athlete(Celebrity):
         info = {
             'bio': WikiAPI().get_bio(self.name),
             'image': GoogleAPI().get_image(self.name),
-            'highlights': GoogleAPI().get_youtube_video(self.name + "highlight", "athlete")
+            'highlights': GoogleAPI().get_youtube_video(self.name + "highlight", "athlete"),
+            'twitter': get_twitter(self.name),
+            'insta': get_insta(self.name)
         }
         return info
+    
+    
+def get_twitter(name):
+    search = name + " twitter"
+    search = urllib.parse.quote_plus(search)
+    url = 'https://google.com/search?q=' + search
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'lxml')
+    for g in soup.find_all(class_ =  'r'):
+        res = g.text.split()[2]
+        twitter_handle = res[2:(len(res)-1)]
+        return twitter_handle
+
+
+
+def get_insta(name):
+    search = name + " instagram"
+    search = urllib.parse.quote_plus(search)
+    url = 'https://google.com/search?q=' + search
+    response = requests.get(url)
+
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    for g in soup.find_all(class_ =  'r'):
+        res = g.text.split()[2]
+        insta_handle = res[2:(len(res)-1)]
+        return insta_handle 
