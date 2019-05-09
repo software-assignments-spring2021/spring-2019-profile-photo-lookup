909dc5d482b29fc2055cd5ee67c244c9c0355b02
Zach Li <47155881+zichenli6@users.noreply.github.com>
2019-05-09 00:25:21 -0400

    Update test_views.py

--- app/server/crawler/tests/test_views.py
+++ app/server/crawler/tests/test_views.py
@@ -7,7 +7,6 @@ from ..views import CelebrityCrawler
 class CelebrityCrawlerTest(APITestCase):
     def test_POST(self):
         names = {"names": ["Bradley Cooper", "Lindsey Graham"]}
-        print(names)
         response = self.client.post("/crawler", json.dumps(names), content_type="application/json")
         self.assertEqual(response.status_code, 200)
 
