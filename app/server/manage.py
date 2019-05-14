c59520608711d053e4e425b5e2571cad9a919d62
Zichen Li <zichenli6@gmail.com>
2019-05-14 12:20:40 -0400

    Update manage.py

--- app/server/manage.py
+++ app/server/manage.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 import os
 import sys
-import dotenv
 
 if __name__ == '__main__':
-    dotenv.read_dotenv() # Load API Keys
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RISE.settings')
     try:
         from django.core.management import execute_from_command_line
