b9f34d77813c842f5f4a719118c84b712a2011e6
Zichen Li <zichenli6@gmail.com>
2019-05-14 12:29:12 -0400

    Update settings.py

--- app/server/RISE/settings.py
+++ app/server/RISE/settings.py
@@ -126,9 +126,12 @@ STATIC_URL = '/static/'
 
 
 # CORS headers
-
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
+    'localhost:3000'
+)
+CORS_ORIGIN_REGEX_WHITELIST = (
     'localhost:3000'
 )
