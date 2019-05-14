0f2b4c7c6ef0dcae212ea8529848ea8a48368f0d
Zichen Li <zichenli6@gmail.com>
2019-05-14 12:32:22 -0400

    Update settings.py

--- app/server/RISE/settings.py
+++ app/server/RISE/settings.py
@@ -130,8 +130,8 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ORIGIN_WHITELIST = (
-    'localhost:3000'
+    'localhost:3000',
 )
 CORS_ORIGIN_REGEX_WHITELIST = (
-    'localhost:3000'
+    'localhost:3000',
 )
