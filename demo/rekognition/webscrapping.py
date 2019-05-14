7f5eea9c755f03093606c94bffd1e0b43de5d53b
Zach Li <47155881+zichenli6@users.noreply.github.com>
2019-05-14 13:06:54 -0400

    Update webscrapping.py

--- demo/rekognition/webscrapping.py
+++ demo/rekognition/webscrapping.py
@@ -1,8 +1,5 @@
-# https://www.thefamouspeople.com/sports-persons.php
-
 from bs4 import BeautifulSoup
 import requests
-
 
 def getPage():
     page = requests.get("https://www.thefamouspeople.com/sports-persons.php")
@@ -15,7 +12,3 @@ def parseBeautifulSoup():
     result = soup.find_all('div', class_='col-md-4 col-sm-4 col-xs-12 catprofiles')
     person_result = soup.find_all('a', {'class':'tileLink'})
     return person_result[:10]
-    #print(soup.prettify())
-
-
-print(parseBeautifulSoup())
